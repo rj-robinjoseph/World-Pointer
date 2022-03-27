c50d54a47aa52ec0cf86c6faccf4a6240c58c538
rj-robinjoseph <robinjoseph452@gmail.com>
2022-03-27 20:06:48 +0530

    Added Config file

--- server/client/src/config.js
+++ server/client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL = "https://world-pointer.herokuapp.com/"
+    baseURL: "https://world-pointer.herokuapp.com/api/"
 })
